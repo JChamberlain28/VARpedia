c2d9fb2e1d5af8090a0db64957ec9e6bc34f905f
gsin387 <52534775+gsin387@users.noreply.github.com>
2019-10-08 16:41:34 +1300

    added creation object

---
+++ src/wikiSpeakGUI/Creation.java
@@ -0,0 +1,31 @@
+package wikiSpeakGUI;
+
+public class Creation {
+	
+	private String name;
+	private String rating = "null";
+	private String creationDate = "null";
+	private String lastViewed = "null";
+
+	public void creation(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public String getCreationDate() {
+		return creationDate;
+	}
+
+	public String getLastViewed() {
+		return lastViewed;
+	}
+	
+	
+}
