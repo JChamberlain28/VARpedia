0179b2b33c6d71094e594970443292f77bfafa78
gsin387 <52534775+gsin387@users.noreply.github.com>
2019-10-24 01:10:36 +1300

    set the default value of the rating slider to be the current rating

--- src/wikiSpeakGUI/rateCreationController.java
+++ src/wikiSpeakGUI/rateCreationController.java
@@ -15,6 +15,14 @@ public class rateCreationController {
 	private static String creation;
 	
 	@FXML
+	private void initialize() {
+		CommandFactory command = new CommandFactory();
+		String output=command.sendCommand("cat \"creations/metadata/"+creation+"/confidenceRating.txt\"", false).get(0);
+		int rate = Integer.parseInt(output.substring(0,1));
+		rating.setValue(rate);
+	}
+	
+	@FXML
 	private void cancelPress(ActionEvent event) {
 		Stage stage = (Stage) cancelButton.getScene().getWindow();
 		stage.close();
